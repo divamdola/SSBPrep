6641c97c53b4643df8daee671e0a79384b9b334e
Divyanshu Amdola <divyanshuamdola01@gmail.com>
2025-09-21 09:48:00 +0530

    updated

--- frontend/my-app-vite/vite.config.js
+++ frontend/my-app-vite/vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+﻿import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    historyApiFallback: true,
     proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:4000',
+      "/api": {
+        target: "http://127.0.0.1:4000",
         changeOrigin: true,
       }
     }
